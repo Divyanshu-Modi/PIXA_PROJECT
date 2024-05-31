2eb0cdca7edfeca108dd2f3edc753ef44b203993
Divyanshu-Modi <divyan.m05@gmail.com>
2024-05-31 13:55:52 +0530

    PIXA: Testing staging firmware
    Change-Id: I92c6c259ada9c740332e2f5ac89b1bef4a3f1a20
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- main/pixa.c
+++ main/pixa.c
@@ -4,14 +4,16 @@
 #define pr_fmt "PIXA"
 
 #include "audio_common.h"
+#include "audio_mem.h"
 #include "audio_pipeline.h"
+
 #include "board.h"
 #include "es7210.h"
-#include "esp_ns.h"
-#include "esp_nsn_models.h"
-#include "esp_nsn_iface.h"
+#include "es8311.h"
+#include "esp_heap_caps.h"
 #include "esp_vad.h"
 #include "esp_wifi.h"
+#include "equalizer.h"
 #include "filter_resample.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
@@ -24,58 +26,82 @@
 #include "pr_log.h"
 #include "raw_stream.h"
 #include "sdkconfig.h"
+#include "freertos/ringbuf.h"
 
-#define RAW_CFG_BUFFER 7 * 2048
+#define RAW_CFG_BUFFER 4 * 2048
 #define PIXA_IP_BUF_MAX 16
-#define READ_TASK_BUF 2048 * sizeof(int16_t)
+#define READ_TASK_BUF 2048 * sizeof(short)
 #define PLINK_MAX 3
+#define PLINK_PA_MAX 3
+#define MQTT_BUFFER_SIZE (65 * 1024)
+
 #define KHZ(hz) (hz * 1000)
+
+#define SPEAKER_I2S_FREQ KHZ(16)
+#define MIC_I2S_FREQ 2500
+
+typedef struct {
+	audio_element_handle_t i2s;
+	audio_element_handle_t rsp;
+	audio_element_handle_t raw;
+	audio_pipeline_handle_t pipe;
+} pixa_device_t;
+
+typedef struct {
+	audio_element_handle_t i2s;
+	// Doesn't support 11 khz
+	// audio_element_handle_t rsp;
+	audio_element_handle_t raw;
+	audio_element_handle_t eq;
+	audio_pipeline_handle_t pipe;
+} pixa_pa_device_t;
+
+#define TOTAL_SIZE 256 * 1024
+static char *data_buffer = NULL;
+static size_t data_buffer_index = 0;
 
 // SSL CERTIFICATE FOR MQTT SERVER
 extern const uint8_t ssl_start[] asm("_binary_isrgrootx1_pem_start");
 extern const uint8_t ssl_end[] asm("_binary_isrgrootx1_pem_end");
 
 static bool ip_got = false;
+bool data_incoming = false;
+pixa_device_t *pixa = NULL;
+pixa_pa_device_t *pixa_pa = NULL;
+esp_mqtt_client_handle_t client;
 
-typedef struct pixa_buf {
-	audio_element_handle_t raw;
-	esp_mqtt_client_handle_t client;
-} pixa_t;
+void mic_init(pixa_device_t *pixa_ai);
+void spkr_init(pixa_pa_device_t *pixa_ai);
 
 void mqtt_task(void *data)
 {
-	vad_handle_t vad_inst = vad_create(VAD_MODE_4);
+	vad_handle_t vad_inst = vad_create(VAD_MODE_3);
 	vad_state_t vad_state = { 0 };
-	srmodel_list_t *model = esp_srmodel_init("model");
 	char pixa_name[PIXA_IP_BUF_MAX] = "";
 	const char *name = "pixa/input/";
-	const char *ns_model = esp_srmodel_filter(model, ESP_NSNET_PREFIX, NULL);
-	ns_handle_t ns_proc = (ns_handle_t)malloc(sizeof(ns_handle_t));
 	int16_t *vad_buff = (int16_t *)malloc(READ_TASK_BUF);
-	int16_t *ovad_buff = (int16_t *)malloc(READ_TASK_BUF);
 	int msg_id = 0;
 	uint8_t index = 0;
-	pixa_t *pbuf = (pixa_t *)data;
-	esp_nsn_iface_t ns = ESP_NSN_HANDLE;
-
-	esp_nsn_data_t *buf = ns.create((char *)ns_model);
-	ns_proc = ns_pro_create(10, 2, 16000);
-
-	pr_info(pr_fmt, "model: %s", ns_model);
 
 	while (1) {
-		raw_stream_read(pbuf->raw, (char *)vad_buff, READ_TASK_BUF);
-		ns.process(buf, vad_buff, ovad_buff);
-		memset(vad_buff, 0, READ_TASK_BUF);
-		ns_process(ns_proc, ovad_buff, vad_buff);
+		if (data_incoming) { 
+			vTaskDelay(20 / portTICK_PERIOD_MS);
+			continue;
+		}
 
+		raw_stream_read(pixa->raw, (char *)vad_buff, READ_TASK_BUF);
 		vad_state = vad_process(vad_inst, (int16_t *)vad_buff, KHZ(16), 30);
 		if (vad_state == VAD_SPEECH) {
 			++index;
 			sprintf(pixa_name, "%s%d", name, index);
-			msg_id = esp_mqtt_client_publish(pbuf->client, pixa_name, ( const char *)vad_buff, READ_TASK_BUF, 1, 0);
-			vTaskDelay(10 / portTICK_PERIOD_MS);
+			msg_id = esp_mqtt_client_publish(client, pixa_name, ( const char *)vad_buff, READ_TASK_BUF, 1, 0);
 			pr_info(pr_fmt,"sent publish successful, msg_id=%d", msg_id);
+		} else if (index) {
+			++index;
+			sprintf(pixa_name, "%s%d", name, index);
+			msg_id = esp_mqtt_client_publish(client, pixa_name, ( const char *)"##", 2, 1, 0);
+			pr_info(pr_fmt,"sent publish successful, msg_id=%d", msg_id);
+			index=0;
 		}
 	}
 }
@@ -112,7 +138,31 @@ static void mqtt_evt_handler(void *args, esp_event_base_t base, int32_t id, void
 			pr_info(pr_fmt,"Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
 		break;
 	case MQTT_EVENT_DATA:
+		if (event->data_len == 2) {
+			pr_warn(pr_fmt, "DATA: %d", event->total_data_len);
+			raw_stream_write(pixa_pa->raw, data_buffer, data_buffer_index);
+			memset(data_buffer, 0, TOTAL_SIZE);
+			data_buffer_index = 0;
+			data_incoming = false;
+		} else if (!data_incoming)
+			data_incoming = true;
+
+		pr_warn(pr_fmt, "datalen: %d", event->data_len);
+		if (data_buffer_index + event->data_len <= TOTAL_SIZE) {
+			memcpy(data_buffer + data_buffer_index, event->data, event->data_len);
+			data_buffer_index += event->data_len;
+		} else {
+			pr_warn(pr_fmt, "Buffer full or data chunk too large");
+			raw_stream_write(pixa_pa->raw, data_buffer, TOTAL_SIZE);
+			memset(data_buffer, 0, TOTAL_SIZE);
+			data_buffer_index = 0;
+			memcpy(data_buffer + data_buffer_index, event->data, event->data_len);
+			data_buffer_index += event->data_len;
+		}
+
+		break;
 	default:
+		data_incoming = false;
 		break;
 	}
 }
@@ -193,6 +243,154 @@ static inline esp_err_t wifi_init(void)
 	return ESP_OK;
 }
 
+void mic_init(pixa_device_t *pixa_ai)
+{
+	const char *plink[PLINK_MAX] = {"i2s", "filter", "raw"};
+	audio_pipeline_cfg_t pline_cfg = DEFAULT_AUDIO_PIPELINE_CONFIG();
+	i2s_stream_cfg_t i2s_cfg = I2S_STREAM_CFG_DEFAULT_WITH_PARA(CODEC_ADC_I2S_PORT, MIC_I2S_FREQ, I2S_DATA_BIT_WIDTH_16BIT, AUDIO_STREAM_READER);
+	rsp_filter_cfg_t rsp_cfg = DEFAULT_RESAMPLE_FILTER_CONFIG();
+	rsp_cfg.src_rate = KHZ(16);
+	/* Uncomment when the microphone's 16 khz audio is understood by the server
+	rsp_cfg.src_rate = KHZ(44.1);
+	rsp_cfg.dest_rate = KHZ(16);
+	*/
+	rsp_cfg.dest_ch = 2;
+	rsp_cfg.max_indata_bytes = READ_TASK_BUF;
+	rsp_cfg.out_len_bytes = READ_TASK_BUF;
+	raw_stream_cfg_t raw_cfg = { AUDIO_STREAM_READER, RAW_CFG_BUFFER };
+	esp_err_t ret = ESP_OK;
+	audio_hal_codec_i2s_iface_t i2s_f_cfg = {
+		.mode = AUDIO_HAL_MODE_MASTER,
+		.fmt = AUDIO_HAL_I2S_DSP,
+		.samples = AUDIO_HAL_44K_SAMPLES,
+		.bits = AUDIO_HAL_BIT_LENGTH_16BITS,
+	};
+
+	// Select mic bias 1 and 2 as the hardware is connected to them
+	es7210_mic_select(ES7210_INPUT_MIC1 | ES7210_INPUT_MIC2);
+	// Set gain to 9 db
+	es7210_adc_set_gain(9);
+	es7210_adc_config_i2s(AUDIO_HAL_CODEC_MODE_ENCODE, &i2s_f_cfg);
+
+	pixa_ai->i2s = i2s_stream_init(&i2s_cfg);
+	if (!pixa_ai->i2s) {
+		pr_err(pr_fmt, "failed to init i2s stream!");
+		return;
+	}
+
+	pixa_ai->rsp = rsp_filter_init(&rsp_cfg);
+	if (!pixa_ai->rsp) {
+		pr_err(pr_fmt, "failed to init filter!");
+		return;
+	}
+
+	pixa_ai->raw = raw_stream_init(&raw_cfg);
+	if (!pixa_ai->raw) {
+		pr_err(pr_fmt, "failed to raw init stream!");
+		return;
+	}
+
+	pixa_ai->pipe = audio_pipeline_init(&pline_cfg);
+	if (!pixa_ai->pipe) {
+		pr_err(pr_fmt, "failed to initialise pipe!");
+		return;
+	}
+
+	ret = audio_pipeline_register(pixa_ai->pipe, pixa_ai->i2s, plink[0]);
+	if (ret) {
+		pr_err(pr_fmt, "pipeline registration for i2s failed");
+		return;
+	}
+
+	ret = audio_pipeline_register(pixa_ai->pipe, pixa_ai->rsp, plink[1]);
+	if (ret) {
+		pr_err(pr_fmt, "pipeline registration for rsp filter failed");
+		return;
+	}
+
+	ret = audio_pipeline_register(pixa_ai->pipe, pixa_ai->raw, plink[2]);
+	if (ret) {
+		pr_err(pr_fmt, "pipeline registration for raw reader failed");
+		return;
+	}
+
+	audio_pipeline_link(pixa_ai->pipe, &plink[0], PLINK_MAX);
+	audio_pipeline_run(pixa_ai->pipe);
+}
+
+void spkr_init(pixa_pa_device_t *pixa_ai)
+{
+	const char *pa_plink[PLINK_PA_MAX] = {"pa_raw", "pa_eq", "pa_i2s"};
+	i2s_stream_cfg_t i2s_cfg = I2S_STREAM_CFG_DEFAULT_WITH_PARA(I2S_NUM_1, SPEAKER_I2S_FREQ, I2S_DATA_BIT_WIDTH_16BIT, AUDIO_STREAM_WRITER);
+	audio_pipeline_cfg_t pline_cfg = DEFAULT_AUDIO_PIPELINE_CONFIG();
+    equalizer_cfg_t eq_cfg = DEFAULT_EQUALIZER_CONFIG();
+    eq_cfg.set_gain = (int[]) {3, 3, 2, 1, 0, 0, 1, 2, 3, 4};
+	// rsp_filter_cfg_t rsp_cfg = DEFAULT_RESAMPLE_FILTER_CONFIG();
+	// rsp_cfg.src_rate = KHZ(16);
+	// rsp_cfg.dest_rate = KHZ(16);
+	// rsp_cfg.max_indata_bytes = TOTAL_SIZE;
+	// rsp_cfg.out_len_bytes = TOTAL_SIZE;
+    raw_stream_cfg_t raw_cfg = { AUDIO_STREAM_WRITER, TOTAL_SIZE };
+	esp_err_t ret = ESP_OK;
+
+	pixa_ai->raw = raw_stream_init(&raw_cfg);
+	if (!pixa_ai->raw) {
+		pr_err(pr_fmt, "failed to raw init stream!");
+		return;
+	}
+
+	pixa_ai->eq = equalizer_init(&eq_cfg);
+	if (!pixa_ai->eq) {
+		pr_err(pr_fmt, "failed to init eq write stream!");
+		return;
+	}
+
+	// pixa_ai->rsp = rsp_filter_init(&rsp_cfg);
+	// if (!pixa_ai->i2s) {
+	// 	pr_err(pr_fmt, "failed to init rsp write stream!");
+	// 	return;
+	// }
+
+	pixa_ai->i2s = i2s_stream_init(&i2s_cfg);
+	if (!pixa_ai->i2s) {
+		pr_err(pr_fmt, "failed to init i2s write stream!");
+		return;
+	}
+
+	pixa_ai->pipe = audio_pipeline_init(&pline_cfg);
+	if (!pixa_ai->pipe) {
+		pr_err(pr_fmt, "failed to initialise pipe!");
+		return;
+	}
+
+	ret = audio_pipeline_register(pixa_ai->pipe, pixa_ai->raw, pa_plink[0]);
+	if (ret) {
+		pr_err(pr_fmt, "pipeline registration for raw reader failed");
+		return;
+	}
+
+	ret = audio_pipeline_register(pixa_ai->pipe, pixa_ai->eq, pa_plink[1]);
+	if (ret) {
+		pr_err(pr_fmt, "pipeline registration for eq reader failed");
+		return;
+	}
+
+	// ret = audio_pipeline_register(pixa_ai->pipe, pixa_ai->rsp, pa_plink[2]);
+	// if (ret) {
+	// 	pr_err(pr_fmt, "pipeline registration for eq reader failed");
+	// 	return;
+	// }
+
+	ret = audio_pipeline_register(pixa_ai->pipe, pixa_ai->i2s, pa_plink[2]);
+	if (ret) {
+		pr_err(pr_fmt, "pipeline registration for i2s reader failed");
+		return;
+	}
+
+	audio_pipeline_link(pixa_ai->pipe, &pa_plink[0], PLINK_PA_MAX);
+	audio_pipeline_run(pixa_ai->pipe);
+}
+
 void app_main(void)
 {
 	esp_mqtt_client_config_t mqtt_cfg = {
@@ -200,23 +398,12 @@ void app_main(void)
 		.broker.verification.certificate = (const char *)ssl_start,
 		.credentials.username = CONFIG_PIXA_MQTT_USERNAME,
 		.credentials.authentication.password = CONFIG_PIXA_MQTT_PASSWORD,
+		.buffer.size = MQTT_BUFFER_SIZE,
 	};
-	const char *plink[PLINK_MAX] = {"i2s", "filter", "raw"};
 	esp_err_t ret = ESP_OK;
-
-	audio_pipeline_cfg_t pline_cfg = DEFAULT_AUDIO_PIPELINE_CONFIG();
-
-	i2s_stream_cfg_t i2s_cfg = I2S_STREAM_CFG_DEFAULT_WITH_PARA(CODEC_ADC_I2S_PORT, AUDIO_HAL_44K_SAMPLES, I2S_DATA_BIT_WIDTH_16BIT, AUDIO_STREAM_READER);
-	rsp_filter_cfg_t rsp_cfg = DEFAULT_RESAMPLE_FILTER_CONFIG();
-	raw_stream_cfg_t raw_cfg = { AUDIO_STREAM_READER, RAW_CFG_BUFFER };
-
-	audio_board_handle_t board = { 0 };
-	audio_pipeline_handle_t pipeline = { 0 };
-
-	audio_element_handle_t filter = { 0 };
-	audio_element_handle_t i2s_stream = { 0 };
-
-	pixa_t *pbuf = (pixa_t *)malloc(sizeof(pixa_t));
+	audio_board_handle_t board = NULL;
+	pixa = (pixa_device_t *)heap_caps_malloc(sizeof(pixa_device_t), MALLOC_CAP_SPIRAM | MALLOC_CAP_DEFAULT);
+	pixa_pa = (pixa_pa_device_t *)heap_caps_malloc(sizeof(pixa_pa_device_t), MALLOC_CAP_SPIRAM | MALLOC_CAP_DEFAULT);
 
 	board = audio_board_init();
 	if (!board) {
@@ -224,74 +411,30 @@ void app_main(void)
 		return;
 	}
 
-	ret = audio_hal_ctrl_codec(board->adc_hal, AUDIO_HAL_CODEC_MODE_ENCODE, AUDIO_HAL_CTRL_START);
-	if (!ret) {
-		pr_err(pr_fmt, "failed to initialise adc hal!");
-		return;
-	}
+	ret = audio_hal_ctrl_codec(board->audio_hal, AUDIO_HAL_CODEC_MODE_ENCODE, AUDIO_HAL_CTRL_START);
+	pr_warn(pr_fmt, "initialise encode hal! ret: %d", ret);
 
-	// Select mic bias 1 and 2 as the hardware is connected to them
-	es7210_mic_select(ES7210_INPUT_MIC1 | ES7210_INPUT_MIC2);
-	// Set gain to 10 db
-	es7210_adc_set_gain(10);
-
-	i2s_stream = i2s_stream_init(&i2s_cfg);
-	if (!i2s_stream) {
-		pr_err(pr_fmt, "failed to init i2s stream!");
-		return;
-	}
-
-	rsp_cfg.src_rate = KHZ(16); // Set the src data rate to 48KHz
-	filter = rsp_filter_init(&rsp_cfg);
-	if (!filter) {
-		pr_err(pr_fmt, "failed to init filter!");
-		return;
-	}
-
-	pbuf->raw = raw_stream_init(&raw_cfg);
-	if (!pbuf->raw) {
-		pr_err(pr_fmt, "failed to raw init stream!");
-		return;
-	}
-
-	pipeline = audio_pipeline_init(&pline_cfg);
-	if (!pipeline) {
-		pr_err(pr_fmt, "failed to initialise pipe!");
-		return;
-	}
-
-	ret = audio_pipeline_register(pipeline, i2s_stream, plink[0]);
-	if (ret) {
-		pr_err(pr_fmt, "pipeline registration for i2s failed");
-		return;
-	}
-
-	ret = audio_pipeline_register(pipeline, filter, plink[1]);
-	if (ret) {
-		pr_err(pr_fmt, "pipeline registration for rsp filter failed");
-		return;
-	}
-
-	ret = audio_pipeline_register(pipeline, pbuf->raw, plink[2]);
-	if (ret) {
-		pr_err(pr_fmt, "pipeline registration for raw reader failed");
-		return;
-	}
-
-	audio_pipeline_link(pipeline, &plink[0], PLINK_MAX);
-	audio_pipeline_run(pipeline);
+	ret = audio_hal_ctrl_codec(board->audio_hal, AUDIO_HAL_CODEC_MODE_DECODE, AUDIO_HAL_CTRL_START);
+	pr_warn(pr_fmt, "initialise decode hal! ret: %d", ret);
 
 	wifi_init();
+	mic_init(pixa);
+	spkr_init(pixa_pa);
 
-	pbuf->client = esp_mqtt_client_init(&mqtt_cfg);
-	if (pbuf->client) {
-		esp_mqtt_client_register_event(pbuf->client, ESP_EVENT_ANY_ID, mqtt_evt_handler, NULL);
-		esp_mqtt_client_start(pbuf->client);
+	// Adjust pa to output the max possible volume
+	audio_hal_set_volume(board->audio_hal, 100);
+
+	client = esp_mqtt_client_init(&mqtt_cfg);
+	if (client) {
+		esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_evt_handler, NULL);
+		esp_mqtt_client_start(client);
 	}
+
+	data_buffer = (char *)heap_caps_malloc(TOTAL_SIZE, MALLOC_CAP_SPIRAM | MALLOC_CAP_DEFAULT);
 
 	pr_info(pr_fmt, "MQTT clinet initialised!");
 
-	xTaskCreate(mqtt_task, "MQTT_TASK", 8192 , pbuf, 0, NULL );
+	xTaskCreatePinnedToCore(mqtt_task, "MQTT_TASK", 8192 , NULL, 1, NULL, 0);
 
 	// LOOP indefinately
 	while (1)
